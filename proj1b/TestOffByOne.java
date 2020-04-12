86d5d2221b471053eb41b5d106d659c5a754cecb
Elliot Michael Adams <elliot@Elliots-MBP.lan1>
2020-04-12 02:38:03 -0400

    Added capital letters test to TestOffByOne

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -11,9 +11,12 @@ public class TestOffByOne {
     public void testEqualChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r','q'));
+        assertTrue(offByOne.equalChars('R','Q'));
+        assertTrue(offByOne.equalChars('&','%'));
 
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('A', 'b'));
     }
 }
